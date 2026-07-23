34881278be35fc6f6ecb8232dd5b08bebe07a8ac
John Clements <ClementsJR@users.noreply.github.com>
2026-07-23 16:18:43 -0500

    Started implementing level start behavior

--- Assets/Scripts/Ship.cs
+++ Assets/Scripts/Ship.cs
@@ -1,9 +1,25 @@
 using UnityEngine;
 
-public class Ship : MonoBehaviour
-{
+public class Ship : MonoBehaviour {
+
+    public ShipPart[] parts;
+
+	[Header("Explosion Params")]
+	public float force = 1f;
+	public float radius = 5f;
+	public Vector3 center;
+
+	public void ExplodeParts() {
+        if (center == null) center = transform.position;
+
+        foreach (var part in parts) {
+            part.Explode(force, center, radius);
+        }
+    }
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    /*void Start()
     {
         
     }
@@ -12,5 +28,5 @@ public class Ship : MonoBehaviour
     void Update()
     {
         
-    }
+    }*/
 }
