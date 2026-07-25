9e231729aef82e3dc54dd797b7e5a33428ea1af0
jbmcmath <jbmcmathcoding@protonmail.com>
2026-07-25 12:28:07 -0500

    Added wall collision logic
    Added logic for when the puck touches a wall, sending puck back to starting position and turning cursor follow off.

--- Assets/Scripts/PuckInteraction.cs
+++ Assets/Scripts/PuckInteraction.cs
@@ -5,6 +5,7 @@ public class PuckInteraction : MonoBehaviour
 {
     private bool isInHand;
     private Rigidbody2D puckRB;
+    private Vector3 startingPos;
 
 
     public void toggleCursorFollow()
@@ -18,9 +19,19 @@ public class PuckInteraction : MonoBehaviour
         puckRB.MovePosition(ScreenPos);
     }
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.transform.parent.name == "walls")
+        {
+            toggleCursorFollow();
+            puckRB.position = startingPos;
+        }
+    }
+
     private void Start()
     {
         puckRB = this.GetComponent<Rigidbody2D>();
+        startingPos = puckRB.position;
     }
 
     private void Update()
