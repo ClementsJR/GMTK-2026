9dc7509a146e142c59f73b6f0f34801baf5d522e
jbmcmath <jbmcmathcoding@protonmail.com>
2026-07-25 16:40:04 -0500

    Mess randomization logic added
    Added logic for making mess spawn in random locations based on screen width and height.

--- Assets/Scripts/MessSpawner.cs
+++ Assets/Scripts/MessSpawner.cs
@@ -8,12 +8,13 @@ public class MessSpawner : MonoBehaviour
     public GameObject messPrefab;
     [SerializeField] private int numOfMessesToSpawn;
     public UnityEvent onMessCleaned;
+    private float Width = Screen.width;
+    private float Height = Screen.height;
 
     public void checkRemainingMess()
     {
         if (this.transform.childCount-1 > 0)
         {
-            Debug.Log($"Not all messes are cleaned. {transform.childCount-1} remain");//TODO remove when done
             return;
         }
         else
@@ -22,12 +23,21 @@ public class MessSpawner : MonoBehaviour
             onMessCleaned.Invoke();
         }
     }
+
+    private void randomizeLocations(GameObject objToMove)
+    {
+        float randomX = Random.Range(-Width / 2f, Width / 2f);
+        float randomY = Random.Range(-Height / 2f, Height / 2f);
+
+        objToMove.transform.localPosition = new Vector3(randomX, randomY);
+    }
     void Start()
     {
         for(int i = 0; i < numOfMessesToSpawn; i++)
         {
             GameObject currMess = Instantiate(messPrefab, this.transform);
             currMess.GetComponent<messInteraction>().onCleanComplete.AddListener(checkRemainingMess);
+            randomizeLocations(currMess);
         }
     }
 }
