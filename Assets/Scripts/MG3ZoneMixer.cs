2e9006b9a5b8fc730567858879d6af865cde57f6
jbmcmath <jbmcmathcoding@protonmail.com>
2026-07-25 12:57:02 -0500

    Changed MG3ZoneMixer to be dynamic
    changed the random range parameter of the zone mixer to randomize based on the number of zones in the zones array.

--- Assets/Scripts/MG3ZoneMixer.cs
+++ Assets/Scripts/MG3ZoneMixer.cs
@@ -3,9 +3,11 @@ using UnityEngine;
 public class MG3ZoneMixer : MonoBehaviour
 {
     [SerializeField] private GameObject[] zones;
+    private int numOfZones;
 
     private void Start()
     {
+        numOfZones = zones.Length;
         for(int i = 0; i < zones.Length; i++)
         {
             if (zones[i].activeInHierarchy == true)
@@ -13,7 +15,7 @@ public class MG3ZoneMixer : MonoBehaviour
                 zones[i].SetActive(false);
             }
         }
-        int randomChoice = UnityEngine.Random.Range(0, 5);
+        int randomChoice = UnityEngine.Random.Range(0, numOfZones);
         zones[randomChoice].SetActive(true);
     }
 }
