b28d7177fecd01c96cecf8afc362ea57aca9424e
John Clements <ClementsJR@users.noreply.github.com>
2026-07-26 03:42:12 -0500

    Working on interaction between player and ship parts

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,32 +1,31 @@
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
-public class Player : MonoBehaviour
-{
+public class Player : MonoBehaviour {
     private bool acceptInput;
     private bool holdingPart;
+    private bool isMoving;
 
-    [SerializeField] private float moveSpeed = 2f;
-    [SerializeField] private float jumpForce = 6f;
-    [SerializeField] private InteractableDetector detector;
+    public float moveSpeed = 2f;
+    public float jumpForce = 6f;
+    public Vector3 handPosition;
 
+    private InteractableDetector detector;
     private Rigidbody rb;
+    private ShipPart heldPart;
 
     private bool jumpQueued;
 
-    private void Awake()
-    {
+    private void Awake() {
         rb = GetComponent<Rigidbody>();
     }
 
-    private void Start()
-    {
+    private void Start() {
         detector = GetComponentInChildren<InteractableDetector>();
         detector.RegisterPlayer(this);
     }
 
-    private void Update()
-    {
+    private void Update() {
         if (!acceptInput)
             return;
 
@@ -37,47 +36,60 @@ public class Player : MonoBehaviour
             Interact();
     }
 
-    private void FixedUpdate()
-    {
+    private void FixedUpdate() {
         if (!acceptInput)
             return;
 
         Move();
     }
 
-    public void EnableMovement()
-    {
+    public void EnableMovement() {
         acceptInput = true;
     }
 
-    public void DisableMovement()
-    {
+    public void DisableMovement() {
         acceptInput = false;
         rb.linearVelocity = Vector3.zero;
     }
 
-    private void Move()
-    {
+    public Vector3 GetHandPosition() {
+        return transform.position + handPosition;
+    }
+
+    private void Move() {
         Vector2 input = InputManager.Current.Move;
 
         Vector3 desiredVelocity = new Vector3(input.x * moveSpeed, rb.linearVelocity.y, input.y * moveSpeed);
 
         rb.linearVelocity = desiredVelocity;
 
-        if (jumpQueued && IsGrounded())
-        {
+        if (jumpQueued && IsGrounded()) {
             rb.linearVelocity = new Vector3(rb.linearVelocity.x, jumpForce, rb.linearVelocity.z);
         }
 
         jumpQueued = false;
+
+        if (isMoving && desiredVelocity.magnitude == 0) isMoving = false;
+        if (!isMoving && desiredVelocity.magnitude > 0) isMoving = true;
     }
 
-    private bool IsGrounded()
-    {
+    private bool IsGrounded() {
         return Physics.Raycast(transform.position, Vector3.down, 1.1f);
     }
 
-    public void Interact()
-    {
-    }
+    public void Interact() {
+        if (!acceptInput) return;
+
+		if (!holdingPart) {
+            heldPart = detector.GetInteractable();
+            if (heldPart == null) return;
+            holdingPart = true;
+			heldPart.PickUp(this);
+		} else {
+            //holdingPart is true
+			heldPart.Drop();
+            heldPart = null;
+			holdingPart = false;
+        }
+	}
 }
--- Assets/Scripts/ShipPart.cs
+++ Assets/Scripts/ShipPart.cs
@@ -2,21 +2,57 @@ using UnityEngine;
 
 public class ShipPart : MonoBehaviour {
 
-	private Vector3 baseRotation;
+	private Vector3 targetRotation;
+	private Vector3 targetPosition;
 	private Rigidbody physicsBody;
+	private bool beingHeld = false;
+	private Player player;
+
+	public Collider bodyCollider;
+	public Collider targetCollider;
 
 	void Start() {
-		baseRotation = transform.eulerAngles;
+		targetRotation = transform.eulerAngles;
+		targetPosition = transform.position;
 		physicsBody = GetComponentInChildren<Rigidbody>();
+	}
+
+	private void FixedUpdate() {
+		if (!beingHeld)
+			return;
+
+		MoveTo(player.GetHandPosition());
+	}
+
+	private void MoveTo(Vector3 newPosition) {
+		physicsBody.transform.position = newPosition;
 	}
 
 	public void Explode(float blastForce, Vector3 blastCenter, float blastRadius) {
 		Vector3 randomAdjustment = Random.onUnitSphere * 0.25f;
 		physicsBody.isKinematic = false;
 		physicsBody.AddExplosionForce(blastForce, blastCenter+randomAdjustment, blastRadius);
+		bodyCollider.enabled = true;
 	}
 
 	public void Settle() {
 		physicsBody.ResetInertiaTensor();
+	}
+
+	public void PickUp(Player player) {
+		this.player = player;
+		beingHeld = true;
+		physicsBody.isKinematic = true;
+		bodyCollider.enabled = false;
+		targetCollider.enabled = true;
+
+		physicsBody.transform.eulerAngles = targetRotation;
+	}
+
+	public void Drop() {
+		beingHeld = false;
+		physicsBody.isKinematic = false;
+		bodyCollider.enabled = true;
+		targetCollider.enabled = false;
 	}
 }
